a1b9435330b81dca166e25e69523db30e7b15a1b
unknown <olgarozhdestvina@gmail.com>
2020-12-05 09:24:15 +0800

    Added some error handlers to the server

--- flask_server.py
+++ flask_server.py
@@ -1,9 +1,18 @@
+"""
+Project assessment
+Data Representation Module GMIT 2020
+Submitted by: Olga Rozhdestvina (Student No: G00387844)
+Lecturer: Andrew Beatty
+"""
+
 # Main server.
 from flask import Flask, render_template
 from dentists.dentists import dentist_table
 from patients.patients import patient_table
 from login.login import log
+from werkzeug.exceptions import BadRequest, NotFound, MethodNotAllowed, InternalServerError
 
+# Flask app.
 app = Flask(__name__, static_folder="main/static", template_folder="main/templates")
 # Secret key to login.
 app.secret_key = 'someKey'
@@ -13,12 +22,29 @@ app.register_blueprint(dentist_table, url_prefix="/dentists")
 app.register_blueprint(patient_table, url_prefix="/patients")
 app.register_blueprint(log, url_prefix="/login")
 
-
 # Main page
 @app.route('/')
 def home():
     return render_template("index.html")
 
-# Turned off the debug mode as it doesn't allow POST (405 error)
+# Error handlers
+@app.errorhandler(BadRequest)
+def handle_bad_request(e):
+    return 'Bad request!', 400, e
+
+@app.errorhandler(NotFound)
+def handle_bad_request(e):
+    return 'Page is not found!', 404, e
+
+@app.errorhandler(MethodNotAllowed)
+def handle_bad_request(e):
+    return 'Method is not allowed!', 404, e
+    
+@app.errorhandler(InternalServerError)
+def handle_bad_request(e):
+    return 'Internal server error!', 500, e
+
+
+# The debug mode is off as it doesn't allow POST (405 error)
 if __name__ == '__main__':
     app.run()
